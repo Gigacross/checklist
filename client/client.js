a5c31938fd088cb65c9001911b8daf13c2a4b909
Gigacross <kang@sordina.net>
2013-09-23 17:44:49 +0800

    added and changed some files

--- client/client.js
+++ client/client.js
@@ -1,14 +1,47 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to checklist3.";
-  };
+      var input = $("<li><input></li>");
 
-  Template.hello.events({
-    'click input' : function () {
+
+  Template.maindocument.events({
+    'click #createList' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+      createList();
+    },
+    'keypress input' : function (enterKey) {
+      //console.log(enterKey);
+    },
+    'keydown input' : function (keyTrigger) {
+      console.log(keyTrigger);
+      if (keyTrigger.keyIdentifier == "Enter") {
+        console.log(keyTrigger.srcElement.parentNode);
+        console.log(keyTrigger);
+
+        //enterKey.srcElement.parentNode.createElement("li");
+        //this.srcElement.parentNode.createElement("li");
+        //$(enterKey.srcElement.parentNode).append("<li><input></li>");
+        var input = $("<li><input></li>");
+        //$('<li><input></li>').appendTo($(enterKey.srcElement.parentNode.nextSibling.)).focus();
+        $(keyTrigger.srcElement.parentNode.parentNode).append(input);
+        input.children().focus();
+
+        }
+      if (keyTrigger.keyCode == 9) {
+        console.log("you pressed the tab key");
+      }
+      if (keyTrigger.shiftKey && keyTrigger.keyCode == 9) {
+        console.log("you pressed the shift tab");
+      }
     }
   });
-}
-	
+
+  function createList(){
+    document.getElementById('chkList').innerHTML = '<ul id="listmaster"><li><input></li> </ul>';
+  }
+  function newItem() {
+    this.createTextNode("li");
+  }
+        $(function(){
+  $('#listmaster').sortable();
+  $('#listmasterÍ').disableSelection();
+ })
