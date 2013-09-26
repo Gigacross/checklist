cae4a12e4fa7667722217ad0b9f6b837302f3307
Gigacross <kang@sordina.net>
2013-09-26 15:02:36 +0800

    more files,more functions

--- client/client.js
+++ client/client.js
@@ -1,4 +1,6 @@
-      var input = $("<li><input></li>");
+appDB = new Meteor.Collection("checklistDB");
+
+var input = $("<li><input></li>");
 
 
   Template.maindocument.events({
@@ -6,24 +8,33 @@
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
-      createList();
+        createList();
+        createListTitle();
+
     },
+    'click h1' : function () {
+      var listTitle = prompt("Please enter a title","Untitled Checklist");
+        if (listTitle != "") {
+          document.getElementsByName('listtitle')[0].innerHTML = listTitle;
+        }
+        else {
+          document.getElementsByName('listtitle')[0].innerHTML = "Untitled Checklist";
+        }
+     },
     'keypress input' : function (enterKey) {
       //console.log(enterKey);
     },
     'keydown input' : function (keyTrigger) {
-      console.log(keyTrigger);
+      //console.log(keyTrigger);
       if (keyTrigger.keyIdentifier == "Enter") {
-        console.log(keyTrigger.srcElement.parentNode);
-        console.log(keyTrigger);
+        console.log($(keyTrigger.srcElement.parentNode).index());
+        newItemId = newItem();
+        //xPos(keyTrigger);
 
-        //enterKey.srcElement.parentNode.createElement("li");
-        //this.srcElement.parentNode.createElement("li");
-        //$(enterKey.srcElement.parentNode).append("<li><input></li>");
-        var input = $("<li><input></li>");
-        //$('<li><input></li>').appendTo($(enterKey.srcElement.parentNode.nextSibling.)).focus();
+        var input = $('<li id='+newItemId+'><input></li>');
         $(keyTrigger.srcElement.parentNode.parentNode).append(input);
         input.children().focus();
+        console.log($(this).index());
 
         }
       if (keyTrigger.keyCode == 9) {
@@ -34,14 +45,50 @@
       }
     }
   });
+  function listInit() {
+    //initialise an empty title and an empty first input field.
 
+  }
+
+  function createListTitle() {
+    var newTitleId = appDB.insert({
+      title : "Untitled Checklist"
+    });
+    document.getElementsByName('listtitle')[0].id = newTitleId;
+    document.getElementsByName('listtitle')[0].value = "untitled document";
+  }
   function createList(){
-    document.getElementById('chkList').innerHTML = '<ul id="listmaster"><li><input></li> </ul>';
+    //1st item is created, update database with 1st item
+    var idInit = newItem();
+    document.getElementById('chkList').innerHTML = '<ul id="listmaster"><li id='+idInit+' ><input></li> </ul>';
   }
   function newItem() {
-    this.createTextNode("li");
+    var idInit = appDB.insert({
+      userid : "",
+      listmaster : "", //the title of this list item
+      position : {
+        x : "",
+        y : ""
+      },
+      content : "",
+      comments : "",
+      shareIds : ""
+    })
+    return idInit;
   }
-        $(function(){
+  function saveItem() {
+
+  }
+  function xPosInit(eventHdlr) { //retrieve the x position
+    var x = eventHdlr.srcElement.parentNode.index();
+    return x;
+  }
+  function yPosInit(eventHdlr) { //retrieve the y position
+    //check for the previous Y value and copies it, if previous Y value doesn't exists, the default is 0;
+  }
+
+$(function(){
   $('#listmaster').sortable();
   $('#listmasterÍ').disableSelection();
- })
+  
+  })
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+  	appDB = new Meteor.Collection("checklistDB");
+
     // code to run on server at startup
   });
 }
