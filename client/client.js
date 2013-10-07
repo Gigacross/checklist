74c04ebaf1ea9bd327e7a224f0d970c0a4e88980
Gigacross <kang@sordina.net>
2013-10-07 20:33:46 +0800

    added some css, added some checklist functions

--- client/client.js
+++ client/client.js
@@ -1,4 +1,6 @@
 appDB = new Meteor.Collection("checklistDB");
+Meteor.subscribe("listRepository");
+Meteor.subscribe("userItems");
 
 var input = $("<li><input></li>");
 
@@ -27,7 +29,8 @@ var input = $("<li><input></li>");
       //console.log(keyTrigger);
       if (keyTrigger.keyIdentifier == "Enter") {
         console.log($(keyTrigger.srcElement.parentNode).index());
-        newItemId = newItem();
+        listMasterId = $('h1').attr('id');
+        newItemId = newItem(listMasterId);
         //xPos(keyTrigger);
 
         var input = $('<li id='+newItemId+'><input></li>');
@@ -51,56 +54,85 @@ var input = $("<li><input></li>");
       })
     },
     'click #createlist' : function (){
-      $('#main').append(Template.slidepane);
-      $('#slidepane').append(Template.ListMasterRender);
-      createList();
-      createListTitle();
-      ListMasterRenderEvents();
-      $("#slidepane").delegate("#close", "click", function slideLeft() {
+      $('#slidepane').remove();
+      $('#main').append(Meteor.render(Template.slidepane));
+      $('#slidepane').append(Meteor.render(Template.ListMasterRender));
+      var listMasterId = createListTitle();
+      createList(listMasterId);
+      
+
+      /*$("#slidepane").delegate("#close", "click", function slideLeft() {
           console.log("slideleft()");
           $("#slidepane").animate({
-          left: '-=600'
-      })
-  })
+            left: '-=600'
+        })
+      })*/
+    },
+    'click #repository' : function () {
+      $('#slidepane').remove();
+      $('#main').append(Meteor.render(Template.slidepane));
+      $('#slidepane').append(Meteor.render(Template.listrepo));
+      console.log("repo pressed");
     }
   });
-  /*Template.slidepane.events({
+  Template.slidepane.events({
     'click #close' : function(){
       console.log("clicked close");
       $("#slidepane").animate({
         left: '-=600'
       })
     }
-  });*/
-  function ListMasterRenderEvents (){
-    document.getElementById("close").onClick = function (e){
-      console.log("closeme");
-      $("#slidepane").animate({
-        left: '-=600'
-      })
-    }
-  }
+  });
   function listInit() {
     //initialise an empty title and an empty first input field.
 
-  }
 
+  }
+  Template.renderExistingList.title = function () {
+    var titlePtr = appDB.findOne({_id : Session.get("currMasterListId")}, {fields : { title:1 }});
+    return titlePtr.title;
+  }
+  Template.renderExistingList.listItems = function () {
+    //determine selected list
+    return appDB.find({content:{$exists:true},listmaster:Session.get("currMasterListId")}).fetch();
+  }
+  Template.listrepo.listsCollection = function (){
+    //return "collection lists repo";
+    return appDB.find( {title : {$exists : true }}, {fields : {title:1}}).fetch();
+    //return Meteor.Collection("checklistDB").find({},{title:1})
+  }
+  Template.listrepo.events({
+    'click' : function () {
+      console.log($(this));
+      Session.set("currMasterListId" ,$(this).attr('_id'));  
+      if ($(this).attr('_id') != "" ) {
+        //generate list with listmaster list
+        $('#slidepane').remove();
+        $('#main').append(Meteor.render(Template.slidepane));
+        $('#slidepane').append(Meteor.render(Template.renderExistingList()));
+      }
+    }
+  })
+  function getCurrentMasterId () {
+
+  }
   function createListTitle() {
     var newTitleId = appDB.insert({
       title : "Untitled Checklist"
     });
     document.getElementsByName('listtitle')[0].id = newTitleId;
     document.getElementsByName('listtitle')[0].value = "untitled document";
+    return newTitleId;
   }
-  function createList(){
+  function createList(listMasterId){
     //1st item is created, update database with 1st item
-    var idInit = newItem();
-    document.getElementById('chkList').innerHTML = '<ul id="listmaster"><li id='+idInit+' ><input></li></ul>';
+    var itemId = newItem(listMasterId);
+    document.getElementById('chkList').innerHTML = '<ul id="listmaster"><li id='+itemId+' ><input></li></ul>';
   }
-  function newItem() {
-    var idInit = appDB.insert({
-      userid : "",
-      listmaster : "", //the title of this list item
+  function newItem(listMasterId) {
+    var itemId = appDB.insert({
+      userid : Meteor.userId(),
+      listmaster : listMasterId, //the title of this list item
       position : {
         x : "",
         y : ""
@@ -109,7 +141,7 @@ var input = $("<li><input></li>");
       comments : "",
       shareIds : ""
     })
-    return idInit;
+    return itemId;
   }
   function saveItem() {
 
@@ -121,14 +153,20 @@ var input = $("<li><input></li>");
   function yPosInit(eventHdlr) { //retrieve the y position
     //check for the previous Y value and copies it, if previous Y value doesn't exists, the default is 0;
   }
-function slideLeft() {
+  function slideLeft() {
     console.log("slideleft()");
     $("#slidepane").animate({
       left: '-=600'
     })
   }
+  function selectedList(listMasterId) {
+    return appDB.find({content:{$exists:true},listMaster:listMasterId})
+  }
+  function selectList(listMasterId) {
+    appDB.find({_id:listMasterId}).fetch();
+    appDB.find({listMaster : listMasterId});
+  }
 $(function(){
   $('#listmaster').sortable();
-  $('#listmasterÍ').disableSelection(); 
-
+  $('#listmasterÍ').disableSelection();
 })
--- server/server.js
+++ server/server.js
@@ -4,5 +4,21 @@ if (Meteor.isServer) {
   	appDB = new Meteor.Collection("checklistDB");
 
     // code to run on server at startup
+    Meteor.publish("listRepository", function (){
+/*    	return appDB.find({}, {
+    		fields : 
+    			{
+    				title:1,
+    				_id:0
+    			}
+    		}
+    	);*/
+    	return appDB.find( {title : {$exists : true }}, {fields : {title:1}})
+    })
+    Meteor.publish("userItems", function (){
+    	return appDB.find( {content : {$exists : true}, userid: Meteor.userId } )
+    })
+
+
   });
 }
